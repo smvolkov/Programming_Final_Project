848dd6359b22121572ac24860f5dbb79016b4c02
Semion Volkov <semen_volkov@mail.ru>
2023-01-06 16:59:09 +0500

    Task2 added

--- FinalProject.cs
+++ FinalProject.cs
@@ -12,12 +12,32 @@ void RecNumbers(int numb)
     if (numb != 1) RecNumbers(numb - 1);
 }
 
-int AskNumber()
+int AskNumber(string numbName)
 {
-    Console.WriteLine("Введите число N: ");
+    Console.WriteLine($"Введите число {numbName}: ");
     int n = int.Parse(Console.ReadLine());
 
     return n;
 }
 
-RecNumbers(AskNumber());
+//RecNumbers(AskNumber("N"));
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int RecSum(int m, int n)
+{
+    int sum = 0;
+
+    if (m == n) return m;
+    else return m + RecSum(m + 1, n);
+}
+
+void ShowSum(int s)
+{
+    Console.WriteLine($"{s}");
+}
+
+ShowSum(RecSum(AskNumber("M"), AskNumber("N")));

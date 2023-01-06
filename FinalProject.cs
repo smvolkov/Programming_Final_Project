876f56af0e5f7b71cc377d93545dbd2d95656909
Semion Volkov <semen_volkov@mail.ru>
2023-01-06 17:17:54 +0500

    Task3 added

--- FinalProject.cs
+++ FinalProject.cs
@@ -40,4 +40,21 @@ void ShowSum(int s)
     Console.WriteLine($"{s}");
 }
 
-ShowSum(RecSum(AskNumber("M"), AskNumber("N")));
+//ShowSum(RecSum(AskNumber("M"), AskNumber("N")));
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else
+    {
+        if((m != 0) && (n == 0)) return Akerman(m - 1, 1);
+        else return Akerman(m - 1, Akerman(m, n - 1));
+    }
+}
+
+ShowSum(Akerman(AskNumber("M"), AskNumber("N")));
